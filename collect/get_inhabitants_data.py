896cf51e05fa0ce9f466704aee0a4c228644b12e
vnikaski <w.skibicka2@student.uw.edu.pl>
2021-07-03 17:27:47 +0200

    collect - exception proofed

--- collect/get_inhabitants_data.py
+++ collect/get_inhabitants_data.py
@@ -7,8 +7,14 @@ def get_inhabitants_data(address: str):
     :param address: str, address of excel file containing inhabitants data
     :return: pd.DataFrame, inhabitants data for further analysis
     """
-
     names = ['age', 'urban', 'rural']
-    df = pd.read_excel(address, header=5, usecols=[0, 4, 7],
-                       names=names).dropna(how='all').reset_index(drop=True)
-    return df
+    while True:
+        try:
+            df = pd.read_excel(address, header=5, usecols=[0, 4, 7],
+                           names=names).dropna(how='all').reset_index(drop=True)
+            return df
+        except ValueError:
+            print('Please check if your data files contains all of the columns listed below:'
+                  '\n-Age column on 0th position'
+                  '\n-Number of inhabitants in urban areas on 4th position'
+                  '\n-Number of inhabitants in rural areas on 7th position')
--- collect/get_school_data.py
+++ collect/get_school_data.py
@@ -4,8 +4,15 @@ import pandas as pd
 def get_school_data(address: str):
 
     """
-    Gets school data given the address
-    :param address: str of file location (*.xlsx)
+    Gets school data of:
+    - 6-digits teritorial code (województwo, powiat, gmina) e.g. 020401 of school location
+    - names of Województwo, Powiat Gmina of school location
+    - type of Gmina the school is in
+    - name of type of Gmina the school is in
+    - number of students in school
+    - number of teachers (full time positions (combined))
+    given the address of the file containing informatory school data
+    :param address: str of file location (*.xlsx, *.xls, *.xlsm, *.xlsb, *.odf, *.ods)
     :return: pd.DataFrame of school data needed for further analysis
     """
 
@@ -14,8 +21,14 @@ def get_school_data(address: str):
             'Nauczyciele niepełnozatrudnieni (stos.pracy)',
             'Nauczyciele niepełnozatrudnieni (w etatach)']
 
-    df = pd.read_excel(address, usecols=cols)
-    df = df.dropna(how='all')
-    df = df.reset_index(drop=True)
-
-    return df
+    while True:
+        try:
+            df = pd.read_excel(address, usecols=cols)
+            df = df.dropna(how='all')
+            df = df.reset_index(drop=True)
+            return df
+        except ValueError:
+            print("Data file does not contain expected columns. Please provide a file containing all of the below:"
+                  "\n'woj', 'pow', 'gm', 'Województwo', 'Powiat', 'Gmina', 'Typ gminy', 'Typ', 'Nazwa typu', "
+                  "'Uczniowie, wychow., słuchacze', 'Nauczyciele pełnozatrudnieni', "
+                  "'Nauczyciele niepełnozatrudnieni (stos.pracy)', 'Nauczyciele niepełnozatrudnieni (w etatach)'")
