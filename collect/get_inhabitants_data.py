d98aa573d201330f95afc3e6518b390bacf53b14
vnikaski <w.skibicka2@student.uw.edu.pl>
2021-07-03 18:06:40 +0200

    collect

--- collect/get_inhabitants_data.py
+++ collect/get_inhabitants_data.py
@@ -8,13 +8,17 @@ def get_inhabitants_data(address: str):
     :return: pd.DataFrame, inhabitants data for further analysis
     """
     names = ['age', 'urban', 'rural']
+
     while True:
         try:
-            df = pd.read_excel(address, header=5, usecols=[0, 4, 7],
-                           names=names).dropna(how='all').reset_index(drop=True)
-            return df
+            df = pd.read_excel(address, header=5, usecols=[0, 4, 7], names=names)
+            break
         except ValueError:
             print('Please check if your data files contains all of the columns listed below:'
                   '\n-Age column on 0th position'
                   '\n-Number of inhabitants in urban areas on 4th position'
                   '\n-Number of inhabitants in rural areas on 7th position')
+
+    df = df.dropna(how='all').reset_index(drop=True)
+
+    return df
--- collect/get_school_data.py
+++ collect/get_school_data.py
@@ -5,12 +5,13 @@ def get_school_data(address: str):
 
     """
     Gets school data of:
-    - 6-digits teritorial code (województwo, powiat, gmina) e.g. 020401 of school location
+    - first 6-digits of teritorial code (województwo, powiat, gmina) e.g. 020401 of school location
     - names of Województwo, Powiat Gmina of school location
     - type of Gmina the school is in
-    - name of type of Gmina the school is in
+    - type of the school (code)
+    - type of the school (name)
     - number of students in school
-    - number of teachers (full time positions (combined))
+    - number of teachers (full time positions (combined)) in school
     given the address of the file containing informatory school data
     :param address: str of file location (*.xlsx, *.xls, *.xlsm, *.xlsb, *.odf, *.ods)
     :return: pd.DataFrame of school data needed for further analysis
@@ -18,17 +19,26 @@ def get_school_data(address: str):
 
     cols = ['woj', 'pow', 'gm', 'Województwo', 'Powiat', 'Gmina', 'Typ gminy', 'Typ', 'Nazwa typu',
             'Uczniowie, wychow., słuchacze', 'Nauczyciele pełnozatrudnieni',
-            'Nauczyciele niepełnozatrudnieni (stos.pracy)',
             'Nauczyciele niepełnozatrudnieni (w etatach)']
 
     while True:
         try:
-            df = pd.read_excel(address, usecols=cols)
-            df = df.dropna(how='all')
-            df = df.reset_index(drop=True)
-            return df
+            df = pd.read_excel(address, usecols=cols,
+                               converters={'woj': '{:0>2}'.format,
+                                           'pow': '{:0>2}'.format,
+                                           'gm': '{:0>2}'.format})
+            break
         except ValueError:
             print("Data file does not contain expected columns. Please provide a file containing all of the below:"
                   "\n'woj', 'pow', 'gm', 'Województwo', 'Powiat', 'Gmina', 'Typ gminy', 'Typ', 'Nazwa typu', "
                   "'Uczniowie, wychow., słuchacze', 'Nauczyciele pełnozatrudnieni', "
-                  "'Nauczyciele niepełnozatrudnieni (stos.pracy)', 'Nauczyciele niepełnozatrudnieni (w etatach)'")
+                  "'Nauczyciele niepełnozatrudnieni (w etatach)'")
+
+    df = df.dropna().reset_index(drop=True)
+
+    df['location_code'] = df['woj'] + df['pow'] + df['gm']
+    df['teachers'] = df['Nauczyciele pełnozatrudnieni'] + df['Nauczyciele niepełnozatrudnieni (w etatach)']
+
+    df = df.drop(columns=['woj', 'pow', 'gm', 'Nauczyciele pełnozatrudnieni', 'Nauczyciele niepełnozatrudnieni (w etatach)'])
+
+    return df
