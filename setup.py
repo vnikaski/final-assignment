571ac53bbcab443aecc968fa3ee57fc0104d4496
vnikaski <w.skibicka2@student.uw.edu.pl>
2021-07-04 21:51:03 +0200

    fulll functionalities corrected + library necessities

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='final_assignment_410952',
     packages=find_packages(include=['final_assignment_410952']),
-    version='0.1.1',
+    version='0.1.2',
     description='NYPD final assignment',
     author='Weronika Skibicka',
     author_email='w.skibicka2@student.uw.edu.pl',
